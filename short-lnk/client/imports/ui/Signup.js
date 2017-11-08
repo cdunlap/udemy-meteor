cb5e10b11801b26bc2afa10c37ce4a91252587e1
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-08 11:00:24 +0000

    Lecture 57

--- short-lnk/client/imports/ui/Signup.js
+++ short-lnk/client/imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
   constructor(props) {
@@ -12,10 +13,12 @@ export default class Signup extends React.Component {
   
   onSubmit (e) {
     e.preventDefault();
+    const email = this.refs.email.value.trim();
+    const password = this.refs.password.value.trim();
     
-    this.setState({
-      error: 'Derp'
-    })
+    Accounts.createUser({email, password}, (err) => {
+      console.log('Signup callback', err);
+    });
   }
   
   render() {
@@ -26,8 +29,8 @@ export default class Signup extends React.Component {
         {this.state.error ? <p>{this.state.error}</p> : undefined}
         
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input name="email" type="email" placeholder="Email Address" />
-          <input name="password" type="password" placeholder="Password" />
+          <input name="email" ref="email" type="email" placeholder="Email Address" />
+          <input name="password" ref="password" type="password" placeholder="Password" />
           <button>Create Account</button>
         </form>
         
