0e98f9763dd25acd85b38b34b993048dbc0da3a4
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-08 10:45:37 +0000

    Lecture 56

--- short-lnk/client/imports/ui/Signup.js
+++ short-lnk/client/imports/ui/Signup.js
@@ -2,10 +2,34 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      error: ''
+    };
+  }
+  
+  onSubmit (e) {
+    e.preventDefault();
+    
+    this.setState({
+      error: 'Derp'
+    })
+  }
+  
   render() {
     return (
       <div>
         <h1>Join Short Lnk</h1>
+        
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input name="email" type="email" placeholder="Email Address" />
+          <input name="password" type="password" placeholder="Password" />
+          <button>Create Account</button>
+        </form>
         
         <Link to="/">Have an account?</Link>
       </div>
