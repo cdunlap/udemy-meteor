ffba899173dc3a4433450ac233f0a3f7f3c965d8
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-09 08:23:41 +0000

    Lecture 68

--- short-lnk/imports/ui/Link.js
+++ short-lnk/imports/ui/Link.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
+import { Links } from '../api/links';
 
 export default class Link extends React.Component {
   onLogout() {
@@ -8,11 +9,31 @@ export default class Link extends React.Component {
     });
   }
   
+  onSubmit(e) {
+    e.preventDefault();
+    
+    const url = this.refs.url.value.trim();
+    
+    if(url) {
+      Links.insert({
+        url
+      });
+      
+      this.refs.url.value = '';
+    }
+  }
+  
   render() {
     return (
       <div>
         <h1>Your Links</h1>
         <button onClick={this.onLogout.bind(this)}>Log Out</button>
+        
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL" />
+          <button type="submit">Add Link</button>
+        </form>
       </div>
     );
   }
