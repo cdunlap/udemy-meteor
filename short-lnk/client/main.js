6e7b4eb06fb6c1baf233e1454ad124fc1f048c80
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-09 05:07:16 +0000

    Lecture 59

--- short-lnk/client/main.js
+++ short-lnk/client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 
 import Signup from './imports/ui/Signup.js';
 import Link from './imports/ui/Link.js';
@@ -9,6 +10,9 @@ import NotFound from './imports/ui/NotFound.js';
 import Login from './imports/ui/Login.js';
 
 window.browserHistory = browserHistory;
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
 
 const routes = (
   <Router history={browserHistory}>
@@ -18,6 +22,21 @@ const routes = (
     <Route path="*" component={NotFound} />
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  const pathName = browserHistory.getCurrentLocation().pathname;
+  const isUnAuthenticatedPage = unauthenticatedPages.includes(pathName);
+  const isAuthenticatedPage = authenticatedPages.includes(pathName);
+  
+  if(isAuthenticated && isUnAuthenticatedPage) {
+    // Redirect to links
+    browserHistory.push('/links');
+  } else if(!isAuthenticated && isAuthenticatedPage) {
+    // Redirect to home
+    browserHistory.push('/');
+  }
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
