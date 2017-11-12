c563efed6d0b3c5101cff8920cba1798c8f6b539
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-12 08:21:11 +0000

    Lecture 89 (really this time)

--- short-lnk/imports/ui/LinksListItem.js
+++ short-lnk/imports/ui/LinksListItem.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
   constructor(props) {
@@ -31,14 +32,24 @@ export default class LinksListItem extends React.Component {
     this.clipboard.destroy();
   }
   
+  renderStats() {
+    const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+    let visitedMessage = null;
+    if(typeof this.props.lastVisitedAt === 'number') {
+      visitedMessage = `(visited ${moment(this.props.lastVisitedAt).fromNow()})`;
+    }
+    return (
+      <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>
+    )
+  }
+  
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
         <p>{this.props.visible.toString()}</p>
-        <p>{this.props.visitedCount}</p>
-        <p>{this.props.lastVisitedAt}</p>
+        {this.renderStats()}
         <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.copied ? 'Copied' : 'Copy'}</button>
         <button onClick={() => {
           Meteor.call('links.setVisibility', this.props._id, !this.props.visible);
