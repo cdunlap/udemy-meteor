ac7b1dd0fc9ab79db550129da420a947e3278593
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-12 04:43:09 +0000

    Lecture 85

--- short-lnk/imports/ui/LinksList.js
+++ short-lnk/imports/ui/LinksList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
 
 import { Links } from '../api/links';
 import LinksListItem from './LinksListItem';
@@ -17,7 +18,9 @@ export default class LinksList extends React.Component {
   componentDidMount() {
     Meteor.subscribe('links');
     this.linksTracker = Tracker.autorun(() => {
-      const links = Links.find().fetch();
+      const links = Links.find({
+        visible: Session.get('showVisible')
+      }).fetch();
       this.setState({
         links
       });
