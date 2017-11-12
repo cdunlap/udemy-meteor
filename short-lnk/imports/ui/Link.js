fb33f752afca5f1ae3a6b53fdf61391e532161e7
Cale Dunlap <cale.dunlap@gmail.com>
2017-11-12 04:56:53 +0000

    Lecture 86

--- short-lnk/imports/ui/Link.js
+++ short-lnk/imports/ui/Link.js
@@ -4,11 +4,13 @@ import { Meteor } from 'meteor/meteor';
 import LinksList from './LinksList';
 import AddLink from './AddLink';
 import PrivateHeader from './PrivateHeader';
+import LinksListFilters from './LinksListFilters';
 
 export default () => {
   return (
     <div>
       <PrivateHeader title="Your Links"/>
+      <LinksListFilters/>
       <LinksList/>
       <AddLink/>
     </div>
